5591fa41f804d34adc7e74fb06a4b81bcba426a5
ddianalim <dianalim1117@gmail.com>
2024-09-28 16:30:52 -0700

    initial commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Diana Lim on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
